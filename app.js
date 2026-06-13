3ed8a7e844ac35cf34a2f7ad47629bcbdf69a17d
asl345 <asori015@ucr.edu>
2026-06-12 22:14:31 -0700

    Modified rendering loop from using nested loops to just using one decoding loop and creating an offscreen canvas to hold that data, then pass it to the real canvas for scaling

--- app.js
+++ app.js
@@ -3063,26 +3063,28 @@ const colorMap = [0X00, 0X08, 0X10, 0X18, 0X20, 0X29, 0X31, 0X39,
     0XC5, 0XCD, 0XD5, 0XDE, 0XE6, 0XEE, 0XF6, 0XFF];
 ctx.canvas.width = width * scale;
 ctx.canvas.height = height * scale;
-const myImageData = ctx.createImageData(width * scale, height * scale);
+ctx.imageSmoothingEnabled = false;
+// Rendering trick to store original resolution (160 * 144) pixels in
+// offscreen canvas and then have the actual canvas scale it up
+const offscreen = document.createElement('canvas');
+offscreen.width = width;
+offscreen.height = height;
+const offscreenCtx = offscreen.getContext("2d");
+const myImageData = offscreenCtx.createImageData(width, height);
+// 32-bit buffer for pixel data decoded into RGBA
+const buf32 = new Uint32Array(myImageData.data.buffer);
 function wrapper() {
-    const data = myImageData.data;
     let frame = machine.getFrame();
-    let index1 = 0;
-    for (let i = 0; i < height; i++) {
-        for (let x = 0; x < scale; x++) {
-            for (let j = 0; j < width; j++) {
-                for (let y = 0; y < scale; y++) {
-                    let index2 = (i * width) + j;
-                    data[(index1 * 4)] = colorMap[(frame[index2] & 0x001F)];
-                    data[(index1 * 4) + 1] = colorMap[(frame[index2] & 0x03E0) >> 5];
-                    data[(index1 * 4) + 2] = colorMap[(frame[index2] & 0x7C00) >> 10];
-                    data[(index1 * 4) + 3] = 255;
-                    index1++;
-                }
-            }
-        }
+    for (let i = 0; i < width * height; i++) {
+        let pixel = frame[i];
+        let r = colorMap[(pixel & 0x001F)];
+        let g = colorMap[(pixel & 0x03E0) >> 5];
+        let b = colorMap[(pixel & 0x7C00) >> 10];
+        // little-endian: 0xAABBGGRR
+        buf32[i] = (255 << 24) | (b << 16) | (g << 8) | r;
     }
-    ctx.putImageData(myImageData, 0, 0);
+    offscreenCtx.putImageData(myImageData, 0, 0);
+    ctx.drawImage(offscreen, 0, 0, width, height, 0, 0, width * scale, height * scale);
 }
 const fileSelector = document.getElementById('file-selector');
 fileSelector.addEventListener('change', (e) => {
