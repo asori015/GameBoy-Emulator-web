b85f246a2378da81f6c7d16f650685d5113ab4d3
asl345 <asori015@ucr.edu>
2026-06-12 22:30:30 -0700

    Got rid of the offscreen canvas, scaling is now done in the html element itself

--- app.js
+++ app.js
@@ -3055,22 +3055,14 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext("2d");
 const width = 160;
 const height = 144;
-const scale = 2;
 let machine;
 const colorMap = [0X00, 0X08, 0X10, 0X18, 0X20, 0X29, 0X31, 0X39,
     0X41, 0X4A, 0X52, 0X5A, 0X62, 0X6A, 0X73, 0X7B,
     0X83, 0X8B, 0X94, 0X9C, 0XA4, 0XAC, 0XB4, 0XBD,
     0XC5, 0XCD, 0XD5, 0XDE, 0XE6, 0XEE, 0XF6, 0XFF];
-ctx.canvas.width = width * scale;
-ctx.canvas.height = height * scale;
-ctx.imageSmoothingEnabled = false;
-// Rendering trick to store original resolution (160 * 144) pixels in
-// offscreen canvas and then have the actual canvas scale it up
-const offscreen = document.createElement('canvas');
-offscreen.width = width;
-offscreen.height = height;
-const offscreenCtx = offscreen.getContext("2d");
-const myImageData = offscreenCtx.createImageData(width, height);
+ctx.canvas.width = width;
+ctx.canvas.height = height;
+const myImageData = ctx.createImageData(width, height);
 // 32-bit buffer for pixel data decoded into RGBA
 const buf32 = new Uint32Array(myImageData.data.buffer);
 function wrapper() {
@@ -3083,8 +3075,7 @@ function wrapper() {
         // little-endian: 0xAABBGGRR
         buf32[i] = (255 << 24) | (b << 16) | (g << 8) | r;
     }
-    offscreenCtx.putImageData(myImageData, 0, 0);
-    ctx.drawImage(offscreen, 0, 0, width, height, 0, 0, width * scale, height * scale);
+    ctx.putImageData(myImageData, 0, 0);
 }
 const fileSelector = document.getElementById('file-selector');
 fileSelector.addEventListener('change', (e) => {
--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,6 @@ const canvas = <HTMLCanvasElement> document.getElementById('canvas');
 const ctx = canvas.getContext("2d")!;
 const width = 160;
 const height = 144;
-const scale = 2;
 let machine: Machine;
 
 const colorMap = [0X00, 0X08, 0X10, 0X18, 0X20, 0X29, 0X31, 0X39,
@@ -12,17 +11,9 @@ const colorMap = [0X00, 0X08, 0X10, 0X18, 0X20, 0X29, 0X31, 0X39,
                   0X83, 0X8B, 0X94, 0X9C, 0XA4, 0XAC, 0XB4, 0XBD,
                   0XC5, 0XCD, 0XD5, 0XDE, 0XE6, 0XEE, 0XF6, 0XFF]
 
-ctx.canvas.width = width * scale;
-ctx.canvas.height = height * scale;
-ctx.imageSmoothingEnabled = false;
-
-// Rendering trick to store original resolution (160 * 144) pixels in
-// offscreen canvas and then have the actual canvas scale it up
-const offscreen = document.createElement('canvas');
-offscreen.width = width;
-offscreen.height = height;
-const offscreenCtx = offscreen.getContext("2d")!;
-const myImageData = offscreenCtx.createImageData(width, height);
+ctx.canvas.width = width;
+ctx.canvas.height = height;
+const myImageData = ctx.createImageData(width, height);
 // 32-bit buffer for pixel data decoded into RGBA
 const buf32 = new Uint32Array(myImageData.data.buffer);
 
@@ -38,9 +29,7 @@ function wrapper(){
         buf32[i] = (255 << 24) | (b << 16) | (g << 8) | r;
     }
 
-    offscreenCtx.putImageData(myImageData, 0, 0);
-    ctx.drawImage(offscreen, 0, 0, width, height,
-                              0, 0, width * scale, height * scale);
+    ctx.putImageData(myImageData, 0, 0);
 }
 
 const fileSelector = <HTMLInputElement> document.getElementById('file-selector');
